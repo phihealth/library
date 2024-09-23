2d88884d174301e57fd45194bf1a55380cb2bff2
Kirk Ouimet <kirk@kirkouimet.com>
2024-09-22 18:57:31 -0600

    Adding default internal routes

---
+++ app/internal/inventory/transfers/page.tsx
@@ -0,0 +1,14 @@
+export const runtime = 'edge'; // Enable server-side rendering
+
+// Dependencies - Next.js
+import { Metadata } from 'next';
+
+// Metadata
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: 'Transfers • Inventory • Internal',
+    };
+}
+
+// Shim the default export from Structure
+export { default } from '@structure/source/internal/pages/inventory/InventoryTransfersPage';
---
+++ app/internal/layout.tsx
@@ -0,0 +1,5 @@
+// Export named exports
+export * from '@structure/source/internal/layouts/InternalLayout';
+
+// Shim the default export from Structure
+export { default } from '@structure/source/internal/layouts/InternalLayout';
